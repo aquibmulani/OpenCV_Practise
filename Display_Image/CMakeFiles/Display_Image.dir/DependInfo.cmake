b145e1d9fe1aceb60d858588a3121a7574e72a92
aquibmulani <aquibmulai@gmail.com>
2020-11-06 17:42:02 -0800

    First pgm directory with CMake

---
+++ Display_Image/CMakeFiles/Display_Image.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aquib/Display_Image/Display_Image.cpp" "/home/aquib/Display_Image/CMakeFiles/Display_Image.dir/Display_Image.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Display_Image/CMakeFiles/Display_Image.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Display_Image.dir/Display_Image.cpp.o"
+  "Display_Image"
+  "Display_Image.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Display_Image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
